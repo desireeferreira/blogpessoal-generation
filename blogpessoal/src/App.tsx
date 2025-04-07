832e4ad0bfce50f6bff0205c0e242658b1ebde1c
Desiree Ferreira <desireeferreira6@gmail.com>
2025-04-07 14:19:26 -0300

    Atualição de pastas

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,25 +1,39 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Footer from './components/footer/Footer'
-import Navbar from './components/Navbar/Navbar'
-import Home from './pages/home/Home'
-import Cadastro from './pages/cadastro/Cadastro'
-import './App.css'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import './App.css'
+// import Footer from './components/footer/Footer'
+// import Navbar from './components/Navbar/Navbar'
+// import Home from './pages/home/Home'
+// import Login from './pages/login/Login'
+// import Cadastro from './pages/cadastro/Cadastro'
 
+// function App() {
+//   return (
+//     <>
+//         <BrowserRouter>
+//           <Navbar />
+//           <div className="min-h-[80vh]">
+//             <Routes>
+//               <Route path="/" element={<Login />} />
+//               <Route path="/home" element={<Home />} />
+//               <Route path="/cadastro" element={<Cadastro />} />
+//               <Route path="/login" element={<Login />} />
+//             </Routes>
+//           </div>
+//           <Footer />
+//         </BrowserRouter>
+//     </>
+//   )
+// }
+
+// export default App
+import './App.css'
+import ListarUsuarios from './components/usuarios/listarusuarios/ListarUsuarios'
 
 function App() {
+
   return (
     <>
-        <BrowserRouter>
-          <Navbar />
-          <div className="min-h-[80vh]">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/" element={<Cadastro />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
+      <ListarUsuarios />
     </>
   )
 }
