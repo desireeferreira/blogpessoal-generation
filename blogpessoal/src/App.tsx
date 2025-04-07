a53627976652a8fa5ed7529e2aa9cbd87d44bb6c
Desiree Ferreira <desireeferreira6@gmail.com>
2025-04-07 11:15:24 -0300

    Atualização do projeto React

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,9 +1,27 @@
-import Home from "./pages/home/Home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+import Home from './pages/home/Home'
+import Cadastro from './pages/cadastro/Cadastro'
+import './App.css'
+
 
 function App() {
-    return (
-        <Home />
-    );
+  return (
+    <>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Cadastro />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
